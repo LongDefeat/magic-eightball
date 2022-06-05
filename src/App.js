3e4de9c7821ae68e133f3465fb4521a66eff5955
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2022-06-05 16:10:52 -0700

    init commit
    springboard exercise using react and state to create magic eightball

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import EightBall from "./EightBall";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title">Ask the Magic Eightball</h1>
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
